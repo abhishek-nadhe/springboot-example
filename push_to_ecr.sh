c09cf420c4a357971449c7c6edbc38ead5fedfdb
abhishekn4 <abhishekn4@kpit.com>
2024-01-03 13:39:12 +0530

    shell script changes

--- push_to_ecr.sh
+++ push_to_ecr.sh
@@ -15,7 +15,7 @@ DOCKER_IMAGE_NAME="springboot"
 # Set your ECR repository details
 ECR_REGION="$AWS_DEFAULT_REGION"
 ECR_ACCOUNT_ID="$AWS_ACCOUNT_ID"
-ECR_REPOSITORY_URI="${ECR_ACCOUNT_ID}.dkr.ecr.${ECR_REGION}.amazonaws.com/kampdevecr"
+ECR_REPOSITORY_NAME="kampdevecr"
 ECR_TAG="springboot-latest123"
 
 # Step 1: Download the Docker image from S3
@@ -25,15 +25,15 @@ aws s3 cp "s3://${S3_BUCKET}/${S3_PATH}/${DOCKER_IMAGE_NAME}.tar.gz" "${GITHUB_W
 docker load -i "${GITHUB_WORKSPACE}/${DOCKER_IMAGE_NAME}.tar.gz"
 
 # Step 3: Tag the Docker image
-docker tag "${DOCKER_IMAGE_NAME}:tag" "${ECR_REPOSITORY_URI}:${ECR_TAG}"
+docker tag "${DOCKER_IMAGE_NAME}:latest" "${ECR_ACCOUNT_ID}.dkr.ecr.${ECR_REGION}.amazonaws.com/${ECR_REPOSITORY_NAME}:${ECR_TAG}"
 
 # Step 4: Authenticate Docker to the ECR registry
 aws ecr get-login-password --region "${ECR_REGION}" | docker login --username AWS --password-stdin "${ECR_ACCOUNT_ID}.dkr.ecr.${ECR_REGION}.amazonaws.com"
 
 # Step 5: Push the Docker image to ECR
-docker push "${ECR_REPOSITORY_URI}:${ECR_TAG}"
+docker push "${ECR_ACCOUNT_ID}.dkr.ecr.${ECR_REGION}.amazonaws.com/${ECR_REPOSITORY_NAME}:${ECR_TAG}"
 
 # Clean up: Remove the local Docker image
-docker rmi "${DOCKER_IMAGE_NAME}:tag"
+docker rmi "${DOCKER_IMAGE_NAME}:latest"
 
 echo "Docker image ${DOCKER_IMAGE_NAME} has been pushed to ECR successfully!"
